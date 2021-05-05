afbf672d6cc5a2e69949e742de2695d7a6082198
Alison Ghedin <alison.ghedin@outlook.com>
2021-05-04 22:46:03 -0300

    sobrescrevendo funcoes

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,24 +1,52 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
 class CursosAPIView(generics.ListCreateAPIView):
+    """
+        View para lista e criação de Cursos
+    """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+        View para update/delete/detalhe de Cursos
+        É necessário pk na chamada da url
+    """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
 class AvaliacoesAPIView(generics.ListCreateAPIView):
+    """
+        View para lista e criação de Avaliações
+    """
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
 
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+        View para update/delete/detalhe de Avaliação
+        É necessário pk na chamada da url
+    """
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
